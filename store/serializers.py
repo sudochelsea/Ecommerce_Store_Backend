a74386d7dd1d0eff60b6b97e96e63bdde5839d66
sudo-chelsea <chelseakowouvi@gmail.com>
2021-03-11 04:44:00 +0000

    Refactor

--- store/serializers.py
+++ store/serializers.py
@@ -1,13 +1,14 @@
 from rest_framework import serializers
 
+
 class UserSerializers(serializers.Serializer):
-    firstname = serializers.CharField(max_length=255)
-    lastname = serializers.CharField(max_length=255)
+    first_name = serializers.CharField(max_length=255)
+    last_name = serializers.CharField(max_length=255)
     email = serializers.EmailField(max_length=254)
 
 
 class ProductSerializers(serializers.Serializer):
-    productname = serializers.CharField(max_length=200)
+    product_name = serializers.CharField(max_length=200)
     price = serializers.CharField(max_digits=5,decimal_places=2)
     discount_price = serializers.FloatField(blank=True,null=True)
     description = serializers.TextField()
